e09faa9afde3cb5125c0916b1979465e264a40d8
Patrik Lindstrom <patriklindstrom@hotmail.com>
2013-12-01 18:53:22 +0100

    First Hello world test

---
+++ ReadMe.txt
@@ -0,0 +1,7 @@
+Trying to learn Selenium
+I recommend downloading the Selenium IDE plug in for Firefox,
+then you can mix recording and export it to C# and then program you test.
+Note that IE requieres a special driver. 
+See http://www.seleniumhq.org/ for more information. 
+Can also recommend pluralsights course on selenium.
+http://pluralsight.com/training/courses/TableOfContents?courseName=selenium
---
+++ TrainingCampWebTest/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+namespace TrainingCampWebTest
+{
+    class Program
+    {
+        private const string IE_DRIVER_PATH = @"D:\Users\Patrik\Documents\GitHub\TrainingCampWebTest\";
+        static void Main(string[] args)
+        {        
+         IWebDriver driver;
+         StringBuilder verificationErrors;
+         string baseURL;
+        
+           // driver = new FirefoxDriver();
+            
+             driver = new InternetExplorerDriver(IE_DRIVER_PATH);
+            
+            baseURL = "http://trainingcampsk.apphb.com/";
+            verificationErrors = new StringBuilder();
+            driver.Navigate().GoToUrl(baseURL + "/Home/index/en");           
+            driver.FindElement(By.Id("name")).Click();
+            
+        }
+    }
+}
