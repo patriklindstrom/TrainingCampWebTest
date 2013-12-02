b4a88194862ecdaf244e03af79073b0d12b0baa6
Patrik Lindström <patrik.lindstrom@lcube.se>
2013-12-02 13:40:31 +0100

    Added some random waits
    and also repeat the testsuite

--- TrainingCampWebTest/Program.cs
+++ TrainingCampWebTest/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -39,14 +40,21 @@ namespace TrainingCampWebTest
 
         public static void SeleniumTestChore()
         {
+            System.Threading.Thread.Sleep(GetNewRandomNumber(0, 1) * 1000);
             IWebDriver driver = new FirefoxDriver();
-            //driver = new InternetExplorerDriver(IE_DRIVER_PATH);          
+            //driver = new InternetExplorerDriver(IE_DRIVER_PATH);   
+            int noOfTestRunTest = 30;
+            for (int i = 0; i < noOfTestRunTest; i++)
+            {         
             TestHomePage(driver, BASE_URL);
             //Change to englisht
             TestHomePageLanguage(driver: driver, baseUrl: BASE_URL, language: "en",
                 firstProjectMeetingText: "FIRST PROJECT MEETING");
+            System.Threading.Thread.Sleep(GetNewRandomNumber(0, 1) * 1000);
             //Change to japanese
-            TestHomePageLanguage(driver: driver, baseUrl: BASE_URL, language: "ja", firstProjectMeetingText: "最初のプロジェクト会議");
+            TestHomePageLanguage(driver: driver, baseUrl: BASE_URL, language: "ja", firstProjectMeetingText: "最初のプロジェクト会議");            
+            System.Threading.Thread.Sleep(GetNewRandomNumber(1, 3) * 1000);
+            }
         }
 
         private static void TestHomePageLanguage(IWebDriver driver, string baseUrl,string language,string firstProjectMeetingText)
@@ -71,6 +79,30 @@ namespace TrainingCampWebTest
             driver.FindElement(By.Id("comments")).Clear();
             driver.FindElement(By.Id("comments")).SendKeys("I want to join this great camp");
             driver.FindElement(By.Id("submit")).Click();
+        }
+        // Method from the linked answer, copy-pasted here for completeness
+        // added the relevant fields because the original code is an override
+        public static Int32 GetNewRandomNumber(Int32 minValue, Int32 maxValue)
+        {
+            RNGCryptoServiceProvider _rng = new RNGCryptoServiceProvider();
+            byte[] uInt32Buffer = new byte[4];
+
+            if (minValue > maxValue)
+                throw new ArgumentOutOfRangeException("minValue");
+            if (minValue == maxValue) return minValue;
+            Int64 diff = maxValue - minValue;
+            while (true)
+            {
+                _rng.GetBytes(uInt32Buffer);
+                UInt32 rand = BitConverter.ToUInt32(uInt32Buffer, 0);
+
+                Int64 max = (1 + (Int64)UInt32.MaxValue);
+                Int64 remainder = max % diff;
+                if (rand < max - remainder)
+                {
+                    return (Int32)(minValue + (rand % diff));
+                }
+            }
         }
     }
 }
